1e763f79fb04bdbcfd8a4b7141f516249e23c25e
Ny Nivoarijaona <nynivoarijaona@gmail.com>
2023-05-06 19:54:26 +0300

    update todos list after create todo

--- client/src/pages/Dashboard/index.tsx
+++ client/src/pages/Dashboard/index.tsx
@@ -1,21 +1,73 @@
 import React, { useEffect, useState } from "react";
 import "./Dashboard.scss";
-import Header from "../../components/Header";
-import FormCreateTodo from "../../components/FormCreateTodo";
 import ListCreatedTodo from "../../components/ListCreatedTodo";
 import axios from "axios";
 import config from "../../config";
 import { UserContext } from "../../App";
 import CardTodo from "../../components/CardTodo";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import Button from "../../components/Button";
+import Input from "../../components/Input";
+import Select from 'react-select';
+import Header from "../../components/Header";
 
 const Dashboard = () => {
     const value = React.useContext(UserContext)
 
+    /* Create Todo */
+    const optionStatus = [
+        { value: 'TODO', label: 'A faire' },
+        { value: 'INPROGRESS', label: 'En cours' },
+        { value: 'DONE', label: 'Términé' }
+    ]
+
+    const [optionUser, setOptionUser] = useState([])
+
+    const [status, setStatus] = useState({ value: 'TODO', label: 'A faire' });
+    const [assigned, setAssigned] = useState({ value: value.id, label: value.name });
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
+    function handleChangeStatus(status: any) {
+        setStatus(status);
+    }
+
+    function handleChangeAssigned(assigned: any) {
+        setAssigned(assigned);
+    }
+
+    useEffect(() => {
+        axios.get(`${config.url_api}users`)
+            .then((res) => {
+                const newTab = res.data.map((item: { _id: any; name: any; }) => {
+                    return { value: item._id, label: item.name }
+                })
+                setOptionUser(newTab)
+            })
+            .catch(() => {
+
+            })
+    }, [])
+
+    const createTodo = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault();
+        axios.post(`${config.url_api}todos`, { title: title, description: description, status: status.value, assigned: assigned.value, created: value.id })
+            .then(() => {
+                toast.success("Tache crée avec succes");
+                fetchAllToDo()
+            })
+            .catch((error) => {
+                toast.error("Erreur lors de la creation de tache");
+            })
+    }
+
+    /*List todo */
     const [todos, setTodos] = useState([]);
     const [todoInProgress, setTodoInProgress] = useState([]);
     const [todoDones, setTodoDones] = useState([]);
 
-    useEffect(() => {
+    const fetchAllToDo = () => {
         axios.get(`${config.url_api}todos-assigned/${value.id}`)
             .then((res) => {
                 setTodos(res.data.filter((item: { status: string; }) => item.status === "TODO"))
@@ -25,6 +77,10 @@ const Dashboard = () => {
             .catch((error) => {
                 console.log(error)
             })
+    }
+
+    useEffect(() => {
+        fetchAllToDo()
     }, [])
 
     return (
@@ -32,7 +88,29 @@ const Dashboard = () => {
             <Header />
             <div className="body-wrapper container">
                 <div className="add-todo-wrapper">
-                    <FormCreateTodo />
+                    <div className='FormCreateTodo'>
+                        <p>Creer une nouvelle tache</p>
+                        <form>
+                            <Input name='title' type='text' placeholder='Titre' value={title} onChange={(e) => setTitle(e.target.value)} />
+                            <textarea rows={4} name='description' placeholder='Description' value={description} onChange={(e) => setDescription(e.target.value)} />
+                            <Select options={optionStatus} placeholder="Status" value={status}
+                                onChange={handleChangeStatus} />
+                            <Select options={optionUser} placeholder="Assigné" value={assigned}
+                                onChange={handleChangeAssigned} />
+                            <Button type='submit' onClick={(e) => createTodo(e)} label='Créer la tache' deco='blue' />
+                        </form>
+                        <ToastContainer
+                            position="bottom-right"
+                            autoClose={3000}
+                            hideProgressBar={false}
+                            newestOnTop={false}
+                            closeOnClick
+                            rtl={false}
+                            pauseOnFocusLoss={false}
+                            draggable={false}
+                            pauseOnHover={false}
+                        />
+                    </div>
                     <ListCreatedTodo />
                 </div>
                 <div className="todo-wrapper">
