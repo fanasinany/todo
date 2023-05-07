32dc2c7c6a3124b3a55cf35a693fd4babbd0e424
Ny Nivoarijaona <nynivoarijaona@gmail.com>
2023-05-07 08:10:32 +0300

    add arrow in card todo

--- client/src/components/CardTodo/index.tsx
+++ client/src/components/CardTodo/index.tsx
@@ -13,6 +13,18 @@ const CardTodo: FC<CardTodoProps> = ({ id, title, description, status, nameCreat
     const value = React.useContext(UserContext)
     return (
         <div className='CardTodo'>
+            <div className='arrow-wrapper'>
+                {(status === "DONE" || status === "INPROGRESS") &&
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="M20 11H7.83l5.59-5.59L12 4l-8 8l8 8l1.41-1.41L7.83 13H20v-2z" /></svg>
+                    </span>
+                }
+                {(status === "TODO" || status === "INPROGRESS") &&
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="m12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8l-8-8z" /></svg>
+                    </span>
+                }
+            </div>
             <h4>{title}</h4>
             <p>{description}</p>
             <p className='created-by'>Crée par <span>{value.name !== nameCreated ? nameCreated : "vous"}</span></p>
