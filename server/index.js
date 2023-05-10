88d54f1709747f3988752573ab4b1dbbe57d15b8
Ny Nivoarijaona <nynivoarijaona@gmail.com>
2023-05-10 21:15:39 +0300

    mandehana eh

--- server/index.js
+++ server/index.js
@@ -1,35 +1,10 @@
 const express = require('express')
-const mongoose = require('mongoose')
-const bcrypt = require("bcrypt")
-const jwt = require("jsonwebtoken")
-require('dotenv').config()
-const auth = require("./auth");
-const Todo = require('./models/todoModel')
-const User = require('./models/userModel')
 const app = express()
-const port = process.env.PORT || 3000
-
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-
-// Curb Cores Error by adding a header here
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-    );
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-    );
-    next();
-});
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+  res.send('Hello, World!')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.listen(3000, () => {
+  console.log('Server listening on port 3000')
 })
